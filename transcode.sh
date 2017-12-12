c663eee8ff5b888f88ca5a70069901941b335f6d
majamee <majamee@yandex.com>
2017-12-12 13:22:25 +0100

    Update transcode.sh

--- transcode.sh
+++ transcode.sh
@@ -29,4 +29,7 @@ ffmpeg -i output/$filename/audio_128k.m4a -acodec copy -vcodec copy -hls_time 2 
 xsltproc --stringparam run_id "segment" /app/mpd-to-m3u8/mpd_to_hls.xsl output/$filename/playlist.mpd > output/$filename/playlist.m3u8
 
 # Cleanup
-rm output/$filename/intermed_1080p.mp4 output/$filename/intermed_720p.mp4 output/$filename/intermed_480p.mp4 output/$filename/audio_128k.m4a
+rm output/$filename/intermed_1080p.mp4 output/$filename/intermed_720p.mp4 output/$filename/intermed_480p.mp4 output/$filename/audio_128k.m4a && \
+
+# Set permissions for newly created files and folders matching the parent folder's permissions
+chown -R `stat -c "%u:%g" ./` output
